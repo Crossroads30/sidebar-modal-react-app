bc688145b2912e82a41b1d37cbfa4cab2853fdb8
crossroads30 <crossroads30@mail.ru>
2024-01-04 15:35:08 +0300

    feat: functionality for modal to show with custom hook

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,15 +1,29 @@
 import React from 'react'
 import { FaTimes } from 'react-icons/fa'
+import { useGlobalContext } from './context'
+
 const Modal = () => {
+  const { showModal, setShowModal } = useGlobalContext()
 	return (
-		<div className='modal-overlay'>
-			<div className='modal-container'>
-				<button className='close-modal-btn' type='button'>
-					<FaTimes />
-				</button>
-				<h3>modal content</h3>
+		<>
+			<button onClick={() => setShowModal(true)} className='btn' type='button'>
+				show modal
+			</button>
+			<div
+				className={!showModal ? 'modal-overlay' : 'modal-overlay show-modal'}
+			>
+				<div className='modal-container'>
+					<button
+						onClick={() => setShowModal(false)}
+						className='close-modal-btn'
+						type='button'
+					>
+						<FaTimes />
+					</button>
+					<h3>modal content</h3>
+				</div>
 			</div>
-		</div>
+		</>
 	)
 }
 
